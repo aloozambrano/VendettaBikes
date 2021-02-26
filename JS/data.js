94600b9efd7af930e1a8c62b6e800dd86efb3d63
Alejandro Loo <aloozambrano@gmail.com>
2021-02-25 21:27:12 -0500

    Eventos

--- JS/data.js
+++ JS/data.js
@@ -1,27 +1,75 @@
 const PRODUCTOS = [{
   "id": 1,
   "modelo": "Triple Triangle",
+  "img": "/CSS/images/img-bicicletas/1-Triple Triangle.jpg",
   "talla": ["47 X-Small", "50 Small", "53 Medium", "56 Large", "58 X-Large"],
   "precio": 2100,
   "stock": 10,
 }, {
   "id": 2,
   "modelo": "Oval",
+  "img": "/CSS/images/img-bicicletas/2-Oval.jpg",
   "talla": ["47 X-Small", "50 Small", "53 Medium", "56 Large", "58 X-Large"],
   "precio": 1800,
   "stock": 7,
 }, {
   "id": 3,
   "modelo": "All Ground",
+  "img": "/CSS/images/img-bicicletas/3-All Ground.jpg",
   "talla": ["47 X-Small", "50 Small", "53 Medium", "56 Large", "58 X-Large"],
   "precio": 1600,
   "stock": 15,
 }, {
   "id": 4,
   "modelo": "Classic 4130",
+  "img": "/CSS/images/img-bicicletas/4-Classic 4130.jpg",
   "talla": ["47 X-Small", "50 Small", "53 Medium", "56 Large", "58 X-Large"],
   "precio": 1500,
   "stock": 6,
 }];
 
-// const tallasDisponibles = ["47 X-Small", "50 Small", "53 Medium", "56 Large", "58 X-Large"];
+
+const ACCESORIOS = [{
+  "id": 1,
+  "img": "/CSS/images/img-accesorios/1-Mochila.jpg",
+  "nombre": "Mochila",
+  "color": ["Púrpura", "Negro"],
+  "precioAcc": 150,
+  "stock": 10,
+}, {
+  "id": 2,
+  "img": "/CSS/images/img-accesorios/2-Cartuchera.jpg",
+  "nombre": "Cartuchera",
+  "color": "Negro",
+  "precioAcc": 30,
+  "stock": 7,
+}, {
+  "id": 3,
+  "nombre": "Gorra",
+  "img": "/CSS/images/img-accesorios/3-Gorra.jpg",
+  "color": ["Púrpura", "Verde", "Amarillo"],
+  "precioAcc": 40,
+  "stock": 15,
+}, {
+  "id": 4,
+  "img": "/CSS/images/img-accesorios/4-Straps.jpg",
+  "nombre": "Straps",
+  "color": "Negro",
+  "precioAcc": 45,
+  "stock": 6,
+}, {
+  "id": 5,
+  "img": "/CSS/images/img-accesorios/5-Polo.jpg",
+  "nombre": "Polo",
+  "color": "Negro",
+  "precioAcc": 50,
+  "stock": 6,
+}, {
+  "id": 6,
+  "img": "/CSS/images/img-accesorios/6-Camisa.jpg",
+  "nombre": "Camisa",
+  "color": ["Blanco", "Violeta"],
+  "precioAcc": 60,
+  "stock": 6,
+}];
+
--- JS/index1.js
+++ JS/index1.js
@@ -1,8 +1,11 @@
-let entradaUsuario = parseInt(prompt("Hola, gracias por visitar Vendetta Bikes \nIngresa el número de acuerdo al modelo de fixie que deseas: \n\n (1) Tiple Triangle \n (2) Oval \n (3) All Ground \n (4) Classic 4130 \n\n (0) Salir"));
+// let entradaUsuario = parseInt(prompt("Hola, gracias por visitar Vendetta Bikes \nIngresa el número de acuerdo al modelo de fixie que deseas: \n\n (1) Tiple Triangle \n (2) Oval \n (3) All Ground \n (4) Classic 4130 \n\n (0) Salir"));
 
+
+// BICICLETAS   BICICLETAS   BICICLETAS   BICICLETAS
 class Bicicleta {
   constructor(datos) {
     this.id = datos.id;
+    this.img = datos.img;
     this.modelo = datos.modelo;
     this.talla = datos.talla;
     this.precio = datos.precio * 1.18;
@@ -10,47 +13,123 @@ class Bicicleta {
   }
 };
 
-// CARRITO DE COMPRAS
-const carrito = [];
+let cardsBicis = [];
+cardsBicis.push(new Bicicleta(PRODUCTOS[0]));
+cardsBicis.push(new Bicicleta(PRODUCTOS[1]));
+cardsBicis.push(new Bicicleta(PRODUCTOS[2]));
+cardsBicis.push(new Bicicleta(PRODUCTOS[3]));
 
-//OBJETOS PRODUCTOS
-const bici1 = new Bicicleta(PRODUCTOS[0]);
-const bici2 = new Bicicleta(PRODUCTOS[1]);
-const bici3 = new Bicicleta(PRODUCTOS[2]);
-const bici4 = new Bicicleta(PRODUCTOS[3]);
+let card_bici = document.getElementById("bic");
 
+for (let listaBici of cardsBicis) {
 
-function seleccionarBicicleta(IDbicicleta) {
-  switch (IDbicicleta) {
-    case 1: carrito.push(bici1); break;
-    case 2: carrito.push(bici2); break;
-    case 3: carrito.push(bici3); break;
-    case 4: carrito.push(bici4); break;
-    default: console.log("El item seleccionado seleccionado es incorrecto"); break;
+  let b = document.createElement("div");
+  b.classList.add("cardAccesorios")
+  b.innerHTML = `<div id="1" class="col">
+                   <div class="card h-100">
+                      <img src="${listaBici.img}">
+                      <div class="card-body">
+                        <h5 class="card-title"><b> ${listaBici.modelo} </b></h5>
+                        <p>Tallas: ${listaBici.talla}</p>
+                        <p>Precio: S/.${listaBici.precio}</p>
+                        <a href="#" class="btn btn-primary">AGREGAR AL CARRITO</a>
+                      </div>
+                    </div>
+                  </div>`;
+
+  card_bici.appendChild(b);
+};
+
+// ACCESORIOS   ACCESORIOS   ACCESORIOS   ACCESORIOS
+class Accesorios {
+  constructor(datos) {
+    this.idAcc = parseInt(datos.idAcc);
+    this.img = datos.img;
+    this.nombre = datos.nombre;
+    this.color = datos.color;
+    this.precioAcc = parseFloat(datos.precioAcc);
   }
-}
+};
 
-while (entradaUsuario != 0) {
+let cardsAcc = [];
+cardsAcc.push(new Accesorios(ACCESORIOS[0]));
+cardsAcc.push(new Accesorios(ACCESORIOS[1]));
+cardsAcc.push(new Accesorios(ACCESORIOS[2]));
+cardsAcc.push(new Accesorios(ACCESORIOS[3]));
+cardsAcc.push(new Accesorios(ACCESORIOS[4]));
+cardsAcc.push(new Accesorios(ACCESORIOS[5]));
 
-  seleccionarBicicleta(entradaUsuario);
-  entradaUsuario = parseInt(prompt("Ingresa el número de acuerdo al modelo de fixie que deseas: \n\n (1) Tiple Triangle \n (2) Oval \n (3) All Ground \n (4) Classic 4130 \n\n (0) Salir"));
-}
+let card_Acc = document.getElementById("acc");
 
-console.log(carrito);
+for (let listaAcc of cardsAcc) {
 
-let productosAgregados = document.getElementById("carritoDeCompras")
+  let e = document.createElement("div");
+  e.classList.add("cardAccesorios")
+  e.innerHTML = `<div id="1" class="col">
+                   <div class="card h-100">
+                      <img src="${listaAcc.img}">
+                      <div class="card-body">
+                        <h5 class="card-title"><b> ${listaAcc.nombre} </b></h5>
+                        <p>Precio: S/.${listaAcc.precioAcc}</p>
+                        <a href="#" class="btn btn-primary">AGREGAR AL CARRITO</a>
+                      </div>
+                    </div>
+                  </div>`;
 
-for (const listaCarrito of carrito) {
-  let contenedor = document.createElement("div");
-  contenedor.classList.add("carritoCard");
-  contenedor.id = "00" + listaCarrito.id;
-  contenedor.innerHTML = `<h3 class="modeloCarrito"> Modelo: ${listaCarrito.modelo}</h3>
-                          <p class="precioCarrito"> Precio: ${listaCarrito.precio}</p>`;
+  card_Acc.appendChild(e);
+};
 
-  productosAgregados.appendChild(contenedor);
-}
+
+// // CARRITO DE COMPRAS
+// const carrito = [];
+
+// //OBJETOS PRODUCTOS
+// const bici1 = new Bicicleta(PRODUCTOS[0]);
+// const bici2 = new Bicicleta(PRODUCTOS[1]);
+// const bici3 = new Bicicleta(PRODUCTOS[2]);
+// const bici4 = new Bicicleta(PRODUCTOS[3]);
+
+
+// function seleccionarBicicleta(IDbicicleta) {
+//   switch (IDbicicleta) {
+//     case 1: carrito.push(bici1); break;
+//     case 2: carrito.push(bici2); break;
+//     case 3: carrito.push(bici3); break;
+//     case 4: carrito.push(bici4); break;
+//     default: console.log("El item seleccionado seleccionado es incorrecto"); break;
+//   }
+// }
+
+// while (entradaUsuario != 0) {
+
+//   seleccionarBicicleta(entradaUsuario);
+//   entradaUsuario = parseInt(prompt("Ingresa el número de acuerdo al modelo de fixie que deseas: \n\n (1) Tiple Triangle \n (2) Oval \n (3) All Ground \n (4) Classic 4130 \n\n (0) Salir"));
+// }
+
+// console.log(carrito);
+
+// let productosAgregados = document.getElementById("carritoDeCompras")
+
+// for (const listaCarrito of carrito) {
+//   let contenedor = document.createElement("div");
+//   contenedor.classList.add("carritoCard");
+//   contenedor.id = "00" + listaCarrito.id;
+//   contenedor.innerHTML = `<h3 class="modeloCarrito"> Modelo: ${listaCarrito.modelo}</h3>
+//                           <p class="precioCarrito"> Precio: ${listaCarrito.precio}</p>`;
+
+//   productosAgregados.appendChild(contenedor);
+// }
 
 
 // BOTÓN BOLETIN FOOTER
-let btnBoletin = document.getElementById("btn_boletin")
-btnBoletin.onclick = () => { alert("Gracias por suscribirte!") };
+let formulario = document.getElementById("boletin");
+formulario.onsubmit = (event) => {
+  event.preventDefault();
+  let dataFormulario = {
+    nombre: event.target.children[2].value,
+    mail: event.target.children[3].value
+  };
+  let padre = document.getElementById("notificacion");
+  padre.innerHTML = `<p>¡${dataFormulario.nombre}!</p>
+                      <p>Gracias por suscribirte a nuestro boletín mensual</p>`;
+};
